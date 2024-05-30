5b53f1835f5bd053b26bcab37320aa187521be19
Deepam <deepamkgupta@gmail.com>
2024-05-30 17:07:45 +0530

    Description of the changes

--- client/src/Components/DashBoard.jsx
+++ client/src/Components/DashBoard.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-
-const Dashboard = ()=>{
-    return(
-       <div>
-        <h1>Dashbaord</h1>
-        <h2>my name is ritik raj</h2>
-       </div>
-    )
-}
+const Dashboard = () => {
+	return (
+		<div>
+			<h1>Dashbaord</h1>
+			<h2>my name is ritikaa raj</h2>
+		</div>
+	);
+};
 export default Dashboard;
